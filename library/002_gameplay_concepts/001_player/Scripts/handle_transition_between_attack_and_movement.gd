13738d8056e8b28b5be823ef2c14888b129563ff
Sem97410 <72381870+Sem97410@users.noreply.github.com>
2025-01-29 15:56:43 +0400

    Add after image meshes

---
+++ library/002_gameplay_concepts/001_player/Scripts/handle_transition_between_attack_and_movement.gd
@@ -0,0 +1,26 @@
+extends Node
+
+@export_multiline var Summary : String
+@export var index_animation_script : FightAnimationIndex
+@export var animation_tree : AnimationTree
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	handle_transition_between_fight_and_movement()
+
+func handle_transition_between_fight_and_movement() -> void : 
+	if index_animation_script.current_reset_countdown > 0 :
+		animation_tree.set("parameters/MiraAnimations/conditions/can_transition", false)
+		print(" Can't transition")
+	else :
+		animation_tree.set("parameters/MiraAnimations/conditions/can_transition", true)
+		print(" Allowed to transition")
+	
+	
