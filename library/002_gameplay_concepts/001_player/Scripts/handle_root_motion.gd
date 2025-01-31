8ed6a1fb27a8a1e4066d3e086979b9990db3e0ff
Sem97410 <72381870+Sem97410@users.noreply.github.com>
2025-01-31 14:50:30 +0400

    Start of working on the root motion

---
+++ library/002_gameplay_concepts/001_player/Scripts/handle_root_motion.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@export var animation_player : AnimationPlayer
+@export var animation_tree : AnimationTree
+@onready var base_state_machine  = animation_tree["parameters/MiraAnimations/playback"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
