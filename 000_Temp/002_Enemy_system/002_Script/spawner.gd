f0c1e374406671464f46ed88f016753ed8ba7a6e
Gano974 <118378863+Gano974@users.noreply.github.com>
2025-02-11 11:49:25 +0400

    work on spawner

--- 000_Temp/002_Enemy_system/002_Script/spawner.gd
+++ 000_Temp/002_Enemy_system/002_Script/spawner.gd
@@ -1,12 +1,25 @@
 extends Node3D
 
-@onready var spawner_timer: Timer = $SpawnerTimer
-@onready var 
+@export var spawner_timer: Timer 
+@export var max_enemies : float # max enemies_spawn
+@onready var enemies_spawned : float = 0 # nombre d'ennemi spawner
 
-const SLIME = preload("res://000_Temp/002_Enemy_system/001_SCENE/slime.tscn")
+const SLIME = preload("res://000_Temp/002_Enemy_system/001_SCENE/slime.tscn")# charge le slime depuis les folder
 
-func _on_spawner_timer_timeout() -> void:
-	print("spawn !")
-	var newEnemy = SLIME.instantiate()
-	get_parent().add_child(newEnemy)
-	newEnemy.global_position = global_position
+
+#fonction propre a godot qui gère automatique le spawn
+#il gère le délai d'apparition en automatique
+func _on_spawner_timer_timeout() -> void: 
+	
+	#condition pour limité le nombre de slime qui est spawner
+	if enemies_spawned  >= max_enemies : # si les ennemis spawned sont superieur ou égal amax_ennemi
+		print("spawn !")
+		spawner_timer.stop() # stope le spawn
+		return
+		
+	var newEnemy = SLIME.instantiate() # variable qui instancie le slime
+	get_parent().add_child(newEnemy) # récupére le noeud qui contien mon spawner et ajoute newEnemy comme enfant de ce node
+	#newEnemy.global_position -> Position de mon ennemi   =  global_position -> position de mon spawner
+	newEnemy.global_position = global_position # place l'ennemy exatement a l'endroit ou se trouve mon spawner
+	
+	enemies_spawned += 1 # incrémentation de l'apparition de mes ennemi 
