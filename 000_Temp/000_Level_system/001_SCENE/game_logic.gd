86354c703ee6a8775b99eaa5f673267ee9b96298
Gano974 <118378863+Gano974@users.noreply.github.com>
2025-02-12 15:25:37 +0400

    finish first step of ennemy

--- 000_Temp/000_Level_system/001_SCENE/game_logic.gd
+++ 000_Temp/000_Level_system/001_SCENE/game_logic.gd
@@ -2,8 +2,9 @@ extends Node
 
 
 
-@export var player_body : PlayerPawn
+@export var player_body : PlayerPawn # référence du player
 
 func _physics_process(delta: float) -> void:
+	# méthode pour 
 	get_tree().call_group("slime_comportement_function","_update_target_position",player_body.global_position)
 	
