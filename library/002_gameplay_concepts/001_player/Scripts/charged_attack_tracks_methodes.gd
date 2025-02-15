102a9f7edf8276b7bb55d04368a1780ad84b38c0
Sem97410 <72381870+Sem97410@users.noreply.github.com>
2025-02-15 17:01:00 +0400

    Beginin of the set of for the charge attack vfx

--- library/002_gameplay_concepts/001_player/Scripts/charged_attack_tracks_methodes.gd
+++ library/002_gameplay_concepts/001_player/Scripts/charged_attack_tracks_methodes.gd
@@ -2,8 +2,19 @@ extends Node
 
 @export var charged_attack_sound : AudioStreamPlayer
 
+@export var charge_attack_broom_dash_vfx : Node3D
+@export var charge_attack_charging : Node3D
+@export var charge_attack_impact : Node3D
+@export var charge_attack_impact_animation_player : AnimationPlayer
+
 
 
 
 func charged_attack_sfx() -> void : 
 	charged_attack_sound.play()
+
+func enable_charge_attack_charging_vfx() -> void : 
+	charge_attack_charging. visible = true
+
+func disable_charge_attack_charging_vfx() -> void : 
+	charge_attack_charging. visible = false
