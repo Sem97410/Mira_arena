896031001787a3d38a0e4fd08e208fc1469cae0a
Sem97410 <72381870+Sem97410@users.noreply.github.com>
2025-02-17 13:42:11 +0400

    Correction two issues
    - First issue : can't use the impact attack of the collider
    - Second issue : camera shake zoom

--- library/002_gameplay_concepts/001_player/Scripts/camera_shake.gd
+++ library/002_gameplay_concepts/001_player/Scripts/camera_shake.gd
@@ -2,28 +2,30 @@ extends Node
 
 class_name CameraShake
 
+@export var shake_fade: float = 10.0
 
-@export var shake_fade : float = 10.0
+var current_shake: float
 
-var current_shake : float
+@export var light_attack_shake: float = 0.1
+@export var charged_attack_shake: float = 0.3
+@export var death_shake: float = 0.3
 
-@export var light_attack_shake : float = 0.1
-@export var charged_attack_shake : float = 0.3
-@export var death_shake : float = 0.3
+@export var camera_position: Camera3D
 
-@export var camera : Camera3D
+var shake_strength: float = 0.0
+var original_position: Vector3  # Stocke la position d'origine
 
-var shake_strength : float = 0.0
+func _ready() -> void:
+	original_position = camera_position.transform.origin  # Sauvegarde la position de base
 
-func trigger_shake() -> void : 
+func trigger_shake() -> void:
 	shake_strength = current_shake
-	#print(shake_strength)
-
 
 func _process(delta: float) -> void:
-	#print("Current camera shake is : ", current_shake)
-		
-	if shake_strength > 0: 
+	if shake_strength > 0:
 		shake_strength = lerp(shake_strength, 0.0, shake_fade * delta)
-		camera.transform.origin = Vector3(randf_range(-shake_strength,shake_strength),randf_range(-shake_strength,shake_strength),0)
-		#camera.offset = Vector3(randf_range(-shake_strength,shake_strength),randf_range(-shake_strength,shake_strength),0)) # Regarder offset H offset et V offset
+		camera_position.transform.origin = original_position + Vector3(
+			randf_range(-shake_strength, shake_strength),
+			randf_range(-shake_strength, shake_strength),
+			0
+		)
