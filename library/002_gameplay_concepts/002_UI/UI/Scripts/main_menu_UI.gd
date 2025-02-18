3d3874f6e5416a8f0d107216a4364ce458f2edbf
Sem97410 <72381870+Sem97410@users.noreply.github.com>
2025-02-18 17:19:58 +0400

    Make modification on video player and add a background image in the background of the pause menu

--- library/002_gameplay_concepts/002_UI/UI/Scripts/main_menu_UI.gd
+++ library/002_gameplay_concepts/002_UI/UI/Scripts/main_menu_UI.gd
@@ -16,6 +16,13 @@ extends Control
 #@export var close_not_ready_pannel_button : Button # $Not_ready_pannel/MarginContainer/VBoxContainer/Close_not_ready_pannel_button
 #-----------------
 
+
+#VIDEO
+@export var video_player : VideoStreamPlayer
+@export var video_player_container : Control
+@export var skip_video_button : Button
+@export var video_length : float = 6.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	first_main_button.grab_focus()
@@ -54,11 +61,29 @@ func _on_stay_in_game_confirmation_button_button_down() -> void:
 #NOT READY BUTTONS
 
 func _on_story_mode_button_down() -> void:
-	not_ready_pannel.visible = true
-	await get_tree().create_timer(1.5).timeout
-	not_ready_pannel.visible = false
+	#not_ready_pannel.visible = true
+	#await get_tree().create_timer(1.5).timeout
+	#not_ready_pannel.visible = false
+	
+	self.visible = false
+	video_player_container.visible = true
+	video_player.play()
+	skip_video_button.grab_focus()
+	await get_tree().create_timer(video_length).timeout
+	stop_video_player()
+	
 
 func _on_settings_button_down() -> void:
 	not_ready_pannel.visible = true
 	await get_tree().create_timer(1.5).timeout
 	not_ready_pannel.visible = false
+
+
+func _on_skip_video_button_button_down() -> void:
+	stop_video_player()
+	
+func stop_video_player() -> void : 
+	video_player.stop()
+	video_player_container.visible = false
+	self.visible = true
+	first_main_button.grab_focus()
