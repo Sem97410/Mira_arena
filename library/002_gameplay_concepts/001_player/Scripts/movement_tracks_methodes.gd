6e1d538c125dc7f071a9c990611a7bee65b6d123
Gano974 <118378863+Gano974@users.noreply.github.com>
2025-03-13 08:16:32 +0400

    add_sound in scene and in title screen

--- library/002_gameplay_concepts/001_player/Scripts/movement_tracks_methodes.gd
+++ library/002_gameplay_concepts/001_player/Scripts/movement_tracks_methodes.gd
@@ -1,5 +1,5 @@
 extends Node
-
+@export var mira_step : AudioStreamPlayer3D
 @export var player : CharacterBody3D 
 @export var movement_script : PlayerMovementScript
 @export var vfx_scene : PackedScene
@@ -22,3 +22,5 @@ func instantiate_foot_step_vfx() -> void :
 	var vfx_instance = vfx_scene.instantiate()  # Crée une instance du VFX
 	movement_vfx_storage.add_child(vfx_instance)  # Ajoute le VFX dans la scène (même parent que le joueur)
 	vfx_instance.global_transform = player.global_transform  # Place le VFX exactement où est le joueur
+	mira_step.play()
+	
