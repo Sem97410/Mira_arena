92f4a1ba6de9920f231b291786fe7f9f0f9fec20
Gano974 <118378863+Gano974@users.noreply.github.com>
2025-03-13 15:03:57 +0400

    repare_deadZone

--- library/002_gameplay_concepts/004_level_system/002_Script/death_zone.gd
+++ library/002_gameplay_concepts/004_level_system/002_Script/death_zone.gd
@@ -2,12 +2,32 @@ extends Area3D
 
 var respawn_position : Vector3
 @export var player : CharacterBody3D
+@onready var  can_save_position : bool = true
 
-func _ready() -> void:
-	respawn_position =  player.global_position
+#func _ready() -> void:
+	#respawn_position =  player.global_position
+	
+func _process(delta: float) -> void:
+	_save_player_position()
+	_allow_save_position()
+
+
+
+		
+func _save_player_position() -> void:
+	if not player.is_on_floor() and can_save_position == true:
+		respawn_position = player.global_position
+		can_save_position = false
+		
+func _allow_save_position() -> void :
+	if player.is_on_floor():
+		can_save_position = true
+	
 	
 
 
-func _on_area_entered(area: Area3D) -> void:
-	if area.is_in_group("player"):
+func _on_body_entered(body: Node3D) -> void:
+	print("je touche l'area")
+	if body.is_in_group("player"):
 		player.global_position = respawn_position
+	
