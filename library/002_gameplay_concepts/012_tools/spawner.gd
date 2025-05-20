a670e3022801ca1be69420bfce8e2685cbdb694e
Sem97410 <72381870+Sem97410@users.noreply.github.com>
2025-05-20 11:35:56 +0400

    clean up  spawner's code

--- library/002_gameplay_concepts/012_tools/spawner.gd
+++ library/002_gameplay_concepts/012_tools/spawner.gd
@@ -2,42 +2,117 @@
 extends Node3D
 class_name Spawner
 
+#----------------------
+
+@export_group("❗Required References❗ ⚠️")
+
+@export_subgroup("Nodes")
 @export var spawner_timer: Timer
-#@export var enemy_scene: PackedScene
-@export var spawnable_enemies : Array[PackedScene]
-
-@export var spawn_range: float = 5.0  
-
+@onready var wave_manager : WaveManager
 @export var spawner_placement_indicator : MeshInstance3D
 @export var spawner_range_indicator : MeshInstance3D
 
-@onready var wave_manager : WaveManager
-@export var spawner_visible_in_game : bool = false
+#----------------------
 
-var enemy_index : int = 0
-#@export var initial_spawn_count: int = 3
-#@export var activation_input: String  # Exemple : "launch_hunter"
-
+@export_subgroup("Enemies")
+@export var spawnable_enemies : Array[PackedScene]
 var enemies_to_spawn: int
 var enemies_spawned: int = 0
+
+#----------------------
+
+@export_subgroup("SpawnBehaviors")
+@export var spawn_range: float = 5.0  
 var is_active := false
+var enemy_array_index : int = 0
+
+#----------------------
+
+@export_subgroup("Debug tools")
+@export var spawner_visible_in_game : bool = false
+
+# --------------------------------------------------------------------------
+
+## BASE FUNCTIONS
 
 func _ready():
-	
-
 	if not Engine.is_editor_hint():
 		toggle_spawner_visibility(spawner_visible_in_game)
-	
+
 	spawner_timer.stop()
 	
 	wave_manager = get_tree().get_first_node_in_group("wave_manager")
 
+#---
 
 func _process(_delta):
 	if Engine.is_editor_hint(): #If we are in the editor
 		_update_spawn_indicator()
 		
 
+# --------------------------------------------------------------------------
+
+## SPAWN BEHAVIOR
+
+#Launch the spawner and active the timer node
+func start_spawning(number_to_spawn: int) -> void:  
+	enemies_spawned = 0
+	enemies_to_spawn = number_to_spawn
+	is_active = true
+	spawner_timer.start()
+
+#---
+
+func _on_timer_timeout() -> void:  
+	
+	if enemies_spawned >= enemies_to_spawn: # stop the spawner when reach the wanted number
+		spawner_timer.stop()
+		is_active = false
+		return
+
+	#If we reach the max number of enemies that can spawn in a scene
+	if wave_manager.enemies_alive_in_wave >= wave_manager.max_enemies_in_the_scene: 
+		return
+	
+	#Security :  if we didn't set up spawner's ennemy
+	if spawnable_enemies.is_empty():  
+		return
+
+	#Select an enemy in the array
+	var enemy_scene_to_spawn = spawnable_enemies[enemy_array_index] 
+	
+	#Prepare to select the next enemy of the array
+	enemy_array_index += 1 
+	
+	#Reset enemies array index in order to create a loop 
+	if enemy_array_index >= spawnable_enemies.size() :
+		enemy_array_index = 0
+	
+	
+	var new_enemy = enemy_scene_to_spawn.instantiate()
+	get_parent().add_child(new_enemy)
+	
+	#Spawn an enemy in a random range arround the spawner
+	new_enemy.global_position = global_position + _random_offset()
+	enemies_spawned += 1
+	
+	##Add an enemy in the wave manager enemies counter
+	wave_manager.enemies_alive_in_wave += 1
+
+#---
+
+func _random_offset() -> Vector3:
+	return Vector3(
+		randf_range(-spawn_range, spawn_range),
+		0,
+		randf_range(-spawn_range, spawn_range)
+	)
+
+# --------------------------------------------------------------------------
+
+## SPAWN TOOLS
+
+#Modify the size of the spawn range indicator
 func _update_spawn_indicator():
 	
 	if not is_instance_valid(spawner_placement_indicator):  #Is the object exist  in the scene/ memory
@@ -49,6 +124,9 @@ func _update_spawn_indicator():
 		mesh.top_radius = spawn_range
 		mesh.bottom_radius = spawn_range
 
+#---
+
+#Show or hide the spawner during the game (for debug purpose)
 func toggle_spawner_visibility(is_visible : bool) -> void : 
 	if is_visible : 
 		spawner_placement_indicator.visible = true
@@ -57,58 +135,7 @@ func toggle_spawner_visibility(is_visible : bool) -> void :
 		spawner_placement_indicator.visible = false
 		spawner_range_indicator.visible = false
 
-func _random_offset() -> Vector3:
-	return Vector3(
-		randf_range(-spawn_range, spawn_range),
-		0,
-		randf_range(-spawn_range, spawn_range)
-	)
-
-func _on_timer_timeout() -> void:
-	if enemies_spawned >= enemies_to_spawn:
-		spawner_timer.stop()
-		is_active = false  # ← Permet de relancer plus tard avec un autre clic
-		return
-#
-	#if not enemy_scene:
-		#return
-	
-	if wave_manager.enemies_alive_in_wave >= wave_manager.max_enemies_in_the_scene:
-		return
-	
-	if spawnable_enemies.is_empty():    #Security
-		return
-	
-	var enemy_scene_to_spawn = spawnable_enemies[enemy_index] #Select an enemy in the array
-	
-	enemy_index += 1 
-	
-	if enemy_index >= spawnable_enemies.size() :
-		enemy_index = 0
-	
-
-	var new_enemy = enemy_scene_to_spawn.instantiate()
-	get_parent().add_child(new_enemy)
-	new_enemy.global_position = global_position + _random_offset()
-	enemies_spawned += 1
-	
-	##Add an enemy in the wave manager enemies counter
-	wave_manager.enemies_alive_in_wave += 1
-
-
-	#print("🧬 Spawned:", new_enemy.name, "@", new_enemy.global_position)
-
-func start_spawning(number_to_spawn: int) -> void: #enemy_scene_to_use: PackedScene,
-	#if not enemy_scene_to_use:
-		#push_warning("❌ No enemy scene passed to spawner.")
-		#return
-#
-	#enemy_scene = enemy_scene_to_use
-	enemies_spawned = 0
-	enemies_to_spawn = number_to_spawn
-	is_active = true
-	spawner_timer.start()
-
+#---
 
 func _on_spawn_range_changed(value: float) -> void:
 	spawn_range = value
