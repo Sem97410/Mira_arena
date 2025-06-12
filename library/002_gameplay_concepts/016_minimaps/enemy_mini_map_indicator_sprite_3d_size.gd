7a66f407b85b184fbae7caede95c29e904b86bb8
Sem97410 <72381870+Sem97410@users.noreply.github.com>
2025-06-12 14:27:17 +0400

    Add visual modification

--- library/002_gameplay_concepts/016_minimaps/enemy_mini_map_indicator_sprite_3d_size.gd
+++ library/002_gameplay_concepts/016_minimaps/enemy_mini_map_indicator_sprite_3d_size.gd
@@ -1,8 +1,10 @@
 extends Sprite3D
 
 var minimap : Control
-@onready var minimap_indicator_close_size : Vector3 = Vector3(0.6,0.6,0.6)
-@onready var minimap_indicator_far_size : Vector3 = Vector3(2.0,2.0,2.0)
+@export var minimap_indicator_close_size : float = 0.6
+@export var minimap_indicator_far_size : float = 1.75
+@onready var minimap_indicator_close_size_vector : Vector3 = Vector3(minimap_indicator_close_size,minimap_indicator_close_size * 2,minimap_indicator_close_size)
+@onready var minimap_indicator_far_size_vector  : Vector3 = Vector3(minimap_indicator_far_size,minimap_indicator_far_size * 2,minimap_indicator_far_size)
 
 func _ready() -> void:
 	minimap = get_tree().get_first_node_in_group("minimap")
@@ -12,6 +14,6 @@ func _process(delta: float) -> void:
 
 func adapte_indicator_minimap_to_size() -> void : 
 	if minimap.is_far_distance :
-		self.scale = minimap_indicator_far_size
+		self.scale = minimap_indicator_far_size_vector
 	else : 
-		self.scale = minimap_indicator_close_size
+		self.scale = minimap_indicator_close_size_vector
