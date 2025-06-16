1be6db212ed60596156357dabdf9bb48793ad48d
Sem97410 <72381870+Sem97410@users.noreply.github.com>
2025-06-16 15:01:48 +0400

    Update indicator model

--- library/002_gameplay_concepts/007_health_item/health_item.gd
+++ library/002_gameplay_concepts/007_health_item/health_item.gd
@@ -8,6 +8,7 @@ var wave_manager : WaveManager
 @onready var potion_was_used : bool = false 
 @onready var player_is_in_health_zone : bool = false
 @export var health_indicator_minimap : Sprite3D
+@export var health_item_visual_effect : Node3D
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
@@ -55,6 +56,7 @@ func activate_health_potion() -> void :
 		return
 	indication_label.text = " +" + str(health_point) + "HP"
 	health_potion_mesh.visible = false
+	health_item_visual_effect.visible = false
 	health_indicator_minimap.visible = false
 	
 	player.player_current_hp += health_point
@@ -67,6 +69,7 @@ func activate_health_potion() -> void :
 func reset_health_item() -> void : 
 	potion_was_used = false
 	health_potion_mesh.visible = true
+	health_item_visual_effect.visible = true
 	health_indicator_minimap.visible = true
 	indication_label.text = "Press [X] to use potion"
 
